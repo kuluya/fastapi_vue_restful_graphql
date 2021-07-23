d256d802420c0a76c4c6b95ff4acc0630e231b43
kuluya <kuluya@gmail.com>
2021-07-23 21:05:40 +0900

    append migration versions.

---
+++ backend/app/db/migrations/versions/2834c30cda4a_create_first_talbes.py
@@ -0,0 +1,37 @@
+"""create_first_tables
+
+Revision ID: 2834c30cda4a
+Revises: 
+Create Date: 2021-07-22 14:53:19.222295
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic
+revision = '2834c30cda4a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def create_customer_table() -> None:
+    op.create_table(
+        'customers',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('first_name', sa.Text, nullable=False),
+        sa.Column('last_name', sa.Text, nullable=False),
+        sa.Column('email', sa.Text, nullable=False),
+        sa.Column('created', sa.TIMESTAMP, server_default=sa.func.now()),
+        sa.Column('modified', sa.TIMESTAMP, server_default=sa.func.now(), server_onupdate=sa.func.current_timestamp())
+    )
+
+
+def upgrade() -> None:
+    create_customer_table()
+
+
+def downgrade() -> None:
+    op.drop_table("customers")
